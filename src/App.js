06b884797ad0fc9c7a7a88f09fbdb5bded603f56
Dara Ganesha <daraganesha@Daras-MBP.fritz.box>
2020-05-27 19:21:05 +0200

    hamburger3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
 			menuopen: false
 		};
 	}
-	handleClick(){
+	handleClick() {
 		this.setState({ ...this.state, menuopen: !this.state.menuopen });
 	}
 
@@ -16,25 +16,20 @@ class App extends React.Component {
 		return (
 			<div className="container">
 				<div className={this.state.menuopen ? 'menu open' : 'menu'} onClick={() => this.handleClick()}>
-					<ul>
-						<li>Menu 1</li>
-						<li>Menu 2</li>
-						<li>Menu 3</li>
-					</ul>
+					
 				</div>
 
 				<div className={this.state.menuopen ? 'main open' : 'main'}>
-					<div 
-						className={this.state.menuopen ? 'hamburger closed' : 'hamburger opened' }
-						onClick={() =>  this.handleClick()}>
+					<div
+						className={this.state.menuopen ? 'hamburger opened' : 'hamburger closed'}
+						onClick={() => this.handleClick()}
+					>
 						<div className="openedsymbol">
 							<span />
 							<span />
 							<span />
 						</div>
-						<div className="closedsymbol">
-							&times;
-						</div>
+						<div className="closedsymbol">&times;</div>
 					</div>
 				</div>
 			</div>

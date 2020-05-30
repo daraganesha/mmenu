cdc5978f847f0d24c9ad18137ece413022d212d0
Dara Ganesha <daraganesha@Daras-MBP.fritz.box>
2020-05-30 14:39:49 +0200

    icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faBaby, faHandPaper } from '@fortawesome/free-solid-svg-icons';
 
 class App extends React.Component {
 	constructor(props) {
@@ -16,7 +18,28 @@ class App extends React.Component {
 		return (
 			<div className="container">
 				<div className={this.state.menuopen ? 'menu open' : 'menu'} onClick={() => this.handleClick()}>
-					
+					<div className="icon">
+						<ul>
+							<li>
+								<div className="icon-container">
+									<FontAwesomeIcon icon={faCoffee} />
+								</div>
+								<span>DRINK COFFEE</span>
+							</li>
+							<li>
+								<div className="icon-container">
+									<FontAwesomeIcon icon={faHandPaper} />
+								</div>
+								<span>HANDPAPER</span>
+							</li>
+							<li>
+								<div className="icon-container">
+									<FontAwesomeIcon icon={faBaby} />
+								</div>
+								<span>HELLO BABY</span>
+							</li>
+						</ul>
+					</div>
 				</div>
 
 				<div className={this.state.menuopen ? 'main open' : 'main'}>
@@ -29,8 +52,19 @@ class App extends React.Component {
 							<span />
 							<span />
 						</div>
+
 						<div className="closedsymbol">&times;</div>
 					</div>
+
+					<p>
+						Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+						ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+						dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
+						sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+						invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
+						justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+						ipsum dolor sit amet.
+					</p>
 				</div>
 			</div>
 		);
